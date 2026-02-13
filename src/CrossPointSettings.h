2199cc8da64680e5834e0881eebb77c6ea975a9c
ztrawhcs <matthew.s.schwartz@gmail.com>
2026-02-13 09:56:04 -0500

    Update CrossPointSettings.h

--- src/CrossPointSettings.h
+++ src/CrossPointSettings.h
@@ -152,7 +152,7 @@ class CrossPointSettings {
   uint8_t refreshFrequency = REFRESH_15;
   uint8_t hyphenationEnabled = 0;
 
-// Reader screen margin settings
+  // Reader screen margin settings
   uint8_t screenMargin = 5;
   // OPDS browser settings
   char opdsServerUrl[128] = "";
